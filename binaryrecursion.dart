1dcf882ab6750b4be37b1c2f3d6650fc95b48ecd
Adhil Fouzi <adhilfouzi@gmail.com>
2023-12-21 10:21:32 +0530

    21/12/23

---
+++ binaryrecursion.dart
@@ -0,0 +1,36 @@
+int binarySearchRecursive(List<int> arr, int target, int low, int high) {
+  if (low <= high) {
+    int mid = (low + high) ~/ 2; // Calculate the middle index
+
+    if (arr[mid] == target) {
+      return mid; // Element found, return its index
+    } else if (arr[mid] < target) {
+      return binarySearchRecursive(
+          arr, target, mid + 1, high); // Search in the right half
+    } else {
+      return binarySearchRecursive(
+          arr, target, low, mid - 1); // Search in the left half
+    }
+  }
+
+  return -1; // Element not found
+}
+
+void main() {
+  // Sorted array
+  List<int> sortedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  // Element to search for
+  int targetElement = 5;
+
+  // Perform recursive binary search
+  int result = binarySearchRecursive(
+      sortedArray, targetElement, 0, sortedArray.length - 1);
+
+  // Display the result
+  if (result != -1) {
+    print("Element $targetElement found at index $result.");
+  } else {
+    print("Element $targetElement not found in the array.");
+  }
+}
