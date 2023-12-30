dac19403521ec57ecfa8c33640c32b98ec35fb74
Adhil Fouzi <adhilfouzi@gmail.com>
2023-12-30 08:56:06 +0530

    30/12/23

--- binaryc.dart
+++ binaryc.dart
@@ -18,6 +18,27 @@ class single {
     tail = newval;
   }
 
+  void addfist(int val) {
+    Node? newval = Node(val);
+    if (head == null) {
+      head = newval;
+    } else {
+      Node? neft = head;
+      head = newval;
+      head!.next = neft;
+    }
+  }
+
+  void addlast(int val) {
+    Node? newval = Node(val);
+    if (head == null) {
+      head = newval;
+    } else {
+      tail!.next = newval;
+    }
+    tail = newval;
+  }
+
   void display() {
     Node? curr = head;
     while (curr != null) {
@@ -31,8 +52,8 @@ void main() {
   single list = single();
   list.add(5);
   list.add(6);
-  list.add(5);
-  list.add(5);
-  list.add(5);
+  list.addfist(7);
+  list.addlast(8);
+  list.add(9);
   list.display();
 }
